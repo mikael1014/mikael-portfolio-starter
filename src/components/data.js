06c265e7461d2ef1538598a974cd330ba51b9eee
mikael1014 <mikael1014@hotmail.com>
2023-12-12 13:54:11 +0100

    update all projects

--- src/components/data.js
+++ src/components/data.js
@@ -20,20 +20,20 @@ export const projectData = [
         title: 'Quotes Generator',
         description:
             'Helps you generate quotes from about 1600 quotes written by different authors . Quotes are automatically copied to your clipboards.',
-        gitHubLink: 'https://github.com/olawanlejoel/random-quote-generator',
+        gitHubLink: 'https://github.com/mikael1014/random-quote-generator',
     },
     {
         id: 4,
         title: 'Password Generator',
         description:
             'Helps you generates random passwords, you can select what you want your password to entail and also you can copy generated password to clipboard.',
-        gitHubLink: 'https://github.com/olawanlejoel/Password-Generator',
+        gitHubLink: 'https://github.com/mikael1014/Password-Generator',
     },
     {
         id: 5,
         title: 'Twitter UI Clone',
         description:
             'Simple Twitter UI clone built with TailwindCSS and Vue Js. This covers only the homepage of Twitter UI. This is cool to get started with TailwindCSS as it helps understand basic concepts.',
-        gitHubLink: 'https://github.com/olawanlejoel/TwitterUI-clone',
+        gitHubLink: 'https://github.com/mikael1014/TwitterUI-clone',
     },
 ];
